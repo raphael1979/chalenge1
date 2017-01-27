3d48c0cf0f6ea2a825c995dbc70fb0a34dfead63
raphael <moudens.raphael@orange.fr>
2017-01-27 16:11:46 +0100

    fonctionnel

--- app.js
+++ app.js
@@ -7,19 +7,29 @@ $(document).ready(
 			dataType: "xml",
 			success: function(xml) 
 			{ 
-				$(xml).find('client').each( function()
-				{
+				$(xml).find('client').each( function(){
 					var id = $(this).attr('id');
 					var nom = $(this).find('nom').text();
-					
-					console.log(nom);
+					//console.log(nom);
 					$('.choix').append($('<option>', { value: id, text: nom}));
 
 				} );
+				$('select').change(function(){
+					//console.log(value);
+					value = this.value;
+					//console.log(value);
+					var x = $(xml).find('#'+value);
+					var nom = (x.find('nom').text());
+					var prenom = (x.find('prenom').text());
+					var age = (x.find('age').text());
+					var metier = (x.find('metier').text());
+					var mail = (x.find('mail').text());
+					var tel = (x.find('tel').text());
 
+					 $('#receiver').html(nom + ' ' + prenom + ' ' +  age + ' ' +  metier + ' ' +  mail + ' ' +  tel);
+				})
 			}
-			//$('.').;
 		}
 		);
 	}
-	);            
+	);          
